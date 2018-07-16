5023fdab46de4245cf01a2203ec2b8d90944afdf
william <willshinji@gmail.com>
2018-07-16 15:15:06 -0300

    prints movie name

--- spider.js
+++ spider.js
@@ -5,4 +5,9 @@ request('http://www.imdb.com/chart/moviemeter', (error, response, body) => {
   if (error) console.log(`Error: ${error}`);
 
   let $ = cheerio.load(body);
+
+  $('.lister-list tr').each(function(){
+    let title = $(this).find('.titleColumn a').text().trim();
+    console.log(title);
+  });
 });
