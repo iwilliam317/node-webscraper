43e6bc07291ee7de364ae8ae1e6a669b04f67275
william <willshinji@gmail.com>
2018-07-16 15:30:38 -0300

    saves top movies into txt file

--- spider.js
+++ spider.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const fs = require('fs');
 
 request('http://www.imdb.com/chart/moviemeter', (error, response, body) => {
   if (error) console.log(`Error: ${error}`);
@@ -9,6 +10,10 @@ request('http://www.imdb.com/chart/moviemeter', (error, response, body) => {
   $('.lister-list tr').each(function(){
     let title = $(this).find('.titleColumn a').text().trim();
     let rating = $(this).find('.imdbRating strong').text().trim();
-    console.log(title, rating);
+    // console.log(title, rating);
+
+    fs.appendFile('imdb.txt', `Título: ${title} | Nota: ${rating} \n`, error => {
+      if (error) throw error;      
+    });
   });
 });
